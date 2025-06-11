ed12d5e28bb7248b2dbfad295730ccf2a73bac67
Datiri <felix.datiri2020@Gmail.com>
2025-06-11 15:20:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function loadComponent(id, url) {
+    fetch(url)
+    .then(res => res.text())
+    .then(html => document.getElementById(id).innerHTML = html);
+}
+loadComponent('header', 'components/header.html');
+loadComponent('footer', 'components/footer.html');
+
+const pageTitles = {
+  '': 'Urbanwash | Your Clothes our care',
+  '/index.html': 'Urbanwash | Your Clothes our care',
+  '/about.html': 'About Us | Urbanwash',
+  '/services.html': 'Our Services | Urbanwash',
+  '/contact.html': 'Contact Us | Urbanwash'
+}, path = window.location.pathname;
+
+if (pageTitles[path]) {
+  document.title = pageTitles[path];
+}
+
+// Set favicon
+function setFavicon(url) {
+    let link = document.querySelector("link[rel~='icon']");
+    if (!link) {
+        link = document.createElement('link');
+        link.rel = 'icon';
+        document.head.appendChild(link);
+    }
+    link.href = url;
+}
+setFavicon('assets/img/favicon.ico');
